98e50372bc16a59b68f1ca92d77acbcf353c78ca
DeepKansara1212 <deepkansara1212@gmail.com>
2025-07-30 14:29:57 +0530

    Dates

---
+++ 12_Date.php
@@ -0,0 +1,10 @@
+<?php
+    echo date("d/m/y") . "<br>";
+    echo date("d-m-y") . "<br>";
+    echo date("d/m/y") . "<br>";
+    echo date("l") . "<br>";
+
+    date_default_timezone_set("Asia/Kolkata");
+
+    echo date("h:i:sa") . "<br>";
+?>
